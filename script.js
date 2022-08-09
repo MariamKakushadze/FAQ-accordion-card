b4cde9651d401c53e3aa869172fa37b1ade32a17
Mariam Kakushadze <mariam.kakushadze3@gmail.com>
2022-08-09 18:18:38 +0400

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const acc = document.getElementsByClassName("question");
-const answer=document.getElementsByClassName("answer");
+let acc = document.getElementsByClassName("question");
+let answer=document.getElementsByClassName("answer");
+
+/*for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let answer = this.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+}*/
 
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", () => {
