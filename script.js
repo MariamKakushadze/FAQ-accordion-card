570aa9d0601c913a061ce772938641a1149f6488
Mariam Kakushadze <mariam.kakushadze3@gmail.com>
2022-08-09 22:43:18 +0400

    made desktop version

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 let acc = document.getElementsByClassName("question");
 let answer=document.getElementsByClassName("answer");
 
-/*for (let i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     let answer = this.nextElementSibling;
-    if (answer.style.display === "block") {
+    if (answer.style.display == "block") {
       answer.style.display = "none";
     } else {
       answer.style.display = "block";
     }
+    
   });
-}*/
-
+}
+/*
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", () => {
+    
       for (let j = 0; j < answer.length; j++) {
           if (i == j) {
-              answer[j].style.display = answer[j].style.display == "block" ? "none" : "block";  
+              answer[j].style.display = answer[j].style.display == "block" ? "none" : "block"; 
           } else {
-              answer[j].style.display = "none";   
+              answer[j].style.display = "none";  
           }
       }
   });
-}
+}*/
