beb7bd321c429a86d56ad4e73e7354f6f9ad2f90
Mariam Kakushadze <mariam.kakushadze3@gmail.com>
2022-08-10 16:13:16 +0400

    made default tab for accordion

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let answer=document.getElementsByClassName("answer");
 
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
+  
     this.classList.toggle("active");
     let answer = this.nextElementSibling;
     if (answer.style.display == "block") {
@@ -10,19 +11,36 @@ for (let i = 0; i < acc.length; i++) {
     } else {
       answer.style.display = "block";
     }
-    
   });
+  acc[0].click();
 }
+
+
 /*
 for (let i = 0; i < acc.length; i++) {
+  
   acc[i].addEventListener("click", () => {
-    
       for (let j = 0; j < answer.length; j++) {
+      
           if (i == j) {
-              answer[j].style.display = answer[j].style.display == "block" ? "none" : "block"; 
-          } else {
+              if( answer[j].style.display == "block" ){
+                
+                answer[j].style.display = "none";
+                }
+                else{
+                   answer[j].style.display ="block"; 
+              }
+                acc[j].style.fontWeight="700";
+                acc[j].style.paddingBottom="0";
+            } 
+            else {
+              acc[j].attributeStyleMap.clear();
               answer[j].style.display = "none";  
-          }
+
+          } 
+           
       }
+      
   });
-}*/
+} 
+*/
